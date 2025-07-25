4a79b4dee09013c03ea8e6cf87a66953013b5933
Kyle Reto <kylereto@gmail.com>
2025-07-25 06:49:24 -0400

    fix: test on /pets/findByStatus now correctly validates all results for all three possible statuses

--- test_pet.py
+++ test_pet.py
@@ -14,14 +14,7 @@ def test_pet_schema():
     # Validate the response schema against the defined schema in schemas.py
     validate(instance=response.json(), schema=schemas.pet)
 
-'''
-TODO: Finish this test by...
-1) Extending the parameterization to include all available statuses
-2) Validate the appropriate response code
-3) Validate the 'status' property in the response is equal to the expected status
-4) Validate the schema for each object in the response
-'''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", [("available"), ("sold"), ("pending")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -29,7 +22,12 @@ def test_find_by_status_200(status):
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+
+    assert response.status_code == 200
+
+    for pet in response.json():
+        validate(instance=pet, schema=schemas.pet)
+        assert pet['status'] == status
 
 '''
 TODO: Finish this test by...
